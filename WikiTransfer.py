d6bc6544b2e5a6fd1039a1817004f3f515db3af5
NathStevo97 <natstephenson15@gmail.com>
2026-08-23 11:10:47 +0100

    feat: initial rewor and uv add

--- WikiTransfer.py
+++ WikiTransfer.py
@@ -1,24 +1,29 @@
 from Wiki import Wiki
 
+
 class WikiTransfer:
-  def __init__(self, currentWiki: Wiki, newWiki: Wiki):
-    self.currentWiki = currentWiki
-    self.newWiki = newWiki
+    def __init__(self, currentWiki: Wiki, newWiki: Wiki):
+        self.currentWiki = currentWiki
+        self.newWiki = newWiki
 
-
-  def transfer(self, path, newPath):
-      # Get source page content and information for "source wiki"
-      try:
-        page = self.currentWiki.get_page(path)
-      except Exception as e:
-        print(e)
-        return
-      # Upload page to target wiki based on target path and "got" page content
-      try:
-        self.newWiki.upload_page(newPath+"/"+page.name, page)
-        print("Page name: {}\n Old Path: {}\n New Path: {}".format(page.name, "/".join(path.split('/')[:-1]), newPath +"/"+page.name))
-      except Exception as e:
-        print(e)
-      # If any subpages are found for the get_page function, repeat the above
-      for subpage in page.childPages:
-        self.transfer(subpage['path'], newPath+"/"+page.name)
+    def transfer(self, path, newPath):
+        # Get source page content and information for "source wiki"
+        try:
+            page = self.currentWiki.get_page(path)
+        except Exception as e:
+            print(f"Error getting page {path} from current wiki: {e}")
+            return
+        # Upload page to target wiki based on target path and "got" page content
+        try:
+            self.newWiki.upload_page(newPath + "/" + page.name, page)
+            print(
+                "Page name: {}\n Old Path: {}\n New Path: {}".format(
+                    page.name, "/".join(path.split("/")[:-1]), newPath + "/" + page.name
+                )
+            )
+        except Exception as e:
+            print(f"Error uploading page {page.name} to new wiki: {e}")
+            return
+        # If any subpages are found for the get_page function, repeat the above
+        for subpage in page.childPages:
+            self.transfer(subpage["path"], newPath + "/" + page.name)
--- main.py
+++ main.py
@@ -1,50 +1,61 @@
 # Imports
-import os
 import argparse
-from dotenv import load_dotenv, find_dotenv
-from Page import Page
+import os
+
+from dotenv import find_dotenv, load_dotenv
+
 from Wiki import AzureDevOpsWiki, ConfluenceWiki
 from WikiTransfer import WikiTransfer
 
 # Load .env file for environment variables
-load_dotenv(find_dotenv()) 
+load_dotenv(find_dotenv())
+
 
 def init_argparse() -> argparse.ArgumentParser:
-  parser = argparse.ArgumentParser(
-    description="Copy Azure Devops Wiki pages from one wiki to another"
-  )
-  parser.add_argument(
-      "path", help="path to page (including page name)"
-  )
-  parser.add_argument(
-      "newPath", help="location to copy wiki page to"
-  )
-#   parser.add_argument(
-#       "currentWikiUrl", help="Current wiki API URL" 
-#   )
-#   parser.add_argument(
-#       "newWikiUrl", help="New wiki API URL" 
-#   )
-  return parser
+    parser = argparse.ArgumentParser(
+        description="Copy Azure Devops Wiki pages from one wiki to another"
+    )
+    parser.add_argument("path", help="path to page (including page name)")
+    parser.add_argument("newPath", help="location to copy wiki page to")
+    #   parser.add_argument(
+    #       "currentWikiUrl", help="Current wiki API URL"
+    #   )
+    #   parser.add_argument(
+    #       "newWikiUrl", help="New wiki API URL"
+    #   )
+    return parser
 
 
 def main():
-  # Load ADO and Confluence Authentication Variables from .env file
-  ADO_USERNAME = os.getenv('ADO_USERNAME')
-  ADO_PAT = os.getenv('ADO_PAT')
-  CONFLUENCE_PAT = os.getenv('CONFLUENCE_PAT')
-  CONFLUENCE_USERNAME = os.getenv('CONFLUENCE_USERNAME')
-  parser = init_argparse()
-  
-  # Define source and target wikis by calling Wiki.py classes where appropriate
-  currentWiki = AzureDevOpsWiki("ifs-ale",  "IFS-Cloud",  "IFS-Cloud", ADO_USERNAME, ADO_PAT)
-  newWiki = ConfluenceWiki("ifsdev", CONFLUENCE_USERNAME, CONFLUENCE_PAT, "IFSCLOUD", "C:/IFS-Cloud.wiki")
+    # Load ADO and Confluence Authentication Variables from .env file
+    ADO_USERNAME = os.getenv("ADO_USERNAME")
+    ADO_PAT = os.getenv("ADO_PAT")
+    CONFLUENCE_PAT = os.getenv("CONFLUENCE_PAT")
+    CONFLUENCE_USERNAME = os.getenv("CONFLUENCE_USERNAME")
+    org_name = os.getenv("ORG_NAME")
+    project_name = os.getenv("PROJECT_NAME")
+    wiki_name = os.getenv("WIKI_NAME")
+    subdomain = os.getenv("SUBDOMAIN")
+    space_key = os.getenv("SPACE_KEY")
+    attachments_directory = os.getenv("ATTACHMENTS_DIRECTORY")
+    parser = init_argparse()
+    source_path = parser.parse_args().path
+    target_path = parser.parse_args().newPath
 
-  # Define transfer object by calling WikiTransfer.py class based on source and target wiki variables
-  transfer = WikiTransfer(currentWiki, newWiki)
+    # Define source and target wikis by calling Wiki.py classes where appropriate
+    currentWiki = AzureDevOpsWiki(
+        org_name, project_name, wiki_name, ADO_USERNAME, ADO_PAT
+    )
+    newWiki = ConfluenceWiki(
+        subdomain, CONFLUENCE_USERNAME, CONFLUENCE_PAT, space_key, attachments_directory
+    )
 
-  # Call transfer function from WikiTransfer.py - if the target wiki is a Confluence space, the last part of the path should be the parent page name
-  transfer.transfer("IFS Cloud Wiki/Documentation Templates", "Documentation Templates")
+    # Define transfer object by calling WikiTransfer.py class based on source and target wiki variables
+    transfer = WikiTransfer(currentWiki, newWiki)
+
+    # Call transfer function from WikiTransfer.py - if the target wiki is a Confluence space, the last part of the path should be the parent page name
+    transfer.transfer(source_path, target_path)
+
 
 if __name__ == "__main__":
-  main()
+    main()
